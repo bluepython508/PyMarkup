74b3da6a998ade88d35b61d8d250e8159698d9a5
Bluepython 508 <bluepython508@gmail.com>
2018-11-15 15:58:53 +0000

    Fix documentation building

--- docs/conf.py
+++ docs/conf.py
@@ -48,7 +48,6 @@ extensions = [
 intersphinx_mapping = {'python': ('https://docs.python.org/3', None)}
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['.templates']
-autodoc_mock_imports = ['dataclasses']
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
 #
